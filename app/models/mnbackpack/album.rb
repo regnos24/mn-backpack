b149629ac503b8f7190a3983c263fc3e0fb10f97
Chris Songer <csonger@gmail.com>
2011-10-11 19:12:09 -0500

    updated documentation

--- app/models/mnbackpack/album.rb
+++ app/models/mnbackpack/album.rb
@@ -1,6 +1,13 @@
 class Mnbackpack::Album
+  
+  def self.get(mnetid)
+    mn = Mnbackpack::Request.new
+    mn.single('Album.GetTracks', {"MnetId" => "#{mnetid}", "IncludeExplicit" => "false", "Pagesize" => "20"})
+  end
+  
   def self.albums(mnetid)
     mn = Mnbackpack::Request.new
     mn.single('Album.GetTracks', {"MnetId" => "#{mnetid}", "IncludeExplicit" => "false", "Pagesize" => "20"})
   end
+  
 end
