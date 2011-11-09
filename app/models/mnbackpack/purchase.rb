75d4b996d6016b5f6253165c9d4a030b8fa2f156
Trevor Hinesley <thinesley@interoconnect.com>
2011-11-09 11:20:55 -0600

    Did some purchase stuff

---
+++ app/models/mnbackpack/purchase.rb
@@ -0,0 +1,44 @@
+class Mnbackpack::Purchase
+  def submit
+    mn = Mnbackpack::Request.new
+    qstr = mn.create({"Method" => "Purchase.UseBalance", "SiteDomain" => "www.award.fm", "UserIP" => "69.139.123.246"}, true)
+    request = Typhoeus::Request.post(qstr,
+                                    :params => {
+                                      "User" => {
+                                        "FirstName" => "Trevor",
+                                        "LastName" => "Hinesley",
+                                        "EmailAddress" => "thinesley@interoconnect.com",
+                                        "BillingAddress" => {
+                                          "Address1" => "103 Lucy Lane",
+                                          "Address2" => "",
+                                          "City" => "Dothan",
+                                          "State" => "AL",
+                                          "PostalCode" => "36303",
+                                          "Country" => "US"
+                                        }
+                                      },
+                                      "Items" => [
+                                        {"MnetID" => "59614957",
+                                        "ItemType" => "Track",
+                                        "Format" => "MP3",
+                                        "Price" => 0.99,
+                                        "Tax" => 0.00}
+                                      ],
+                                      "TotalCharge" => 0.99
+                                    }
+                                    # , :disable_ssl_peer_verification => true
+                                    )
+    # hydra = Typhoeus::Hydra.new
+    #     hydra.queue(request)
+    #     hydra.run
+    #     response = request.response
+    #     puts response.code    # http status code
+    #     puts response.time    # time in seconds the request took
+    #     puts response.headers # the http headers
+    #     puts response.headers_hash # http headers put into a hash
+    #     puts response.body    # the response body
+    #     response
+    Rails.logger.info qstr
+    request
+  end
+end
